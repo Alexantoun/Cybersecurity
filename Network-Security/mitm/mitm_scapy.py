97c18b24f68c71d3f0475764e120fc9f5c167867
Alexantoun <oz.antoun@gmail.com>
2022-11-06 19:23:06 -0500

    Fixed lack of connection in get_MAC()

--- Network-Security/mitm/mitm_scapy.py
+++ Network-Security/mitm/mitm_scapy.py
@@ -1,28 +1,42 @@
 from async_timeout import timeout
-from scapy.all import * 
+from scapy.all import *
 import sys, time
 
+
 def getMAC(ip): #Ether has 3 fields, 2 of those are dst and src, which contain the MAC addr of destination and source
-    arp_packet = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip)
-    answered, _ = srp(arp_packet, timeout=2, retry=5) #sends packets and recieves answers on layer 2. returns answered and unanswered
-    print(f'Getting MAC address for {ip}')
-    # print(f'sources address is {answered[Ether].dst}')
-    answered[0].show()
-    for rcv in answered:
-        if rcv[Ether].src != None:
-            return rcv[Ether].src #return source MAC addresses
+    print(f'Getting MAC address for {ip} on interface {conf.iface}: ', end="")
+    arp_packet = Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip)#The sequence of 'F' is a reserved broadcast address
+    ans = None
+    counter = 0
+    while ans == None and counter < 10:
+        ans = srp1(arp_packet, verbose=False)#sends packets and recieves answers on layer 2. returns answered and unanswered
+        counter += 1
+        
+    if ans == None:
+        print("Failed to get response")
+        return None
+    else:
+        print(ans[Ether].src)
+        return ans[Ether].src
+
+def spoof_target(target_IP, target_MAC, spoof_ipv4):
+    packet = ARP
+
 
 def main(target_IP:str, host_IP:str):
-    print(f'Target IP = {target_IP}, host IP = {host_IP}')
+    print(f'Target IP = {target_IP}, host IP = {host_IP}\n')
     victim = getMAC(target_IP)
-    print(f'Mac of victim device is: {victim}')
     host = getMAC(host_IP)
-    print(f'Mac of host device is: {host}')
 
-
+    # # victim.show()
+    # host = getMAC(host_IP)
+    # print(f'Mac of host device is: {host}')
 
 if __name__ == '__main__':
+    conf.iface = "eth0"
     if len(sys.argv) < 3:
         print(f'Missing arguments, need 2 but got {len(sys.argv)-1}')
+    elif sys.argv[1] == sys.argv[2]:
+        print("Cannot target two identical IPv4 addresses")
     else:
         main(sys.argv[1], sys.argv[2])
